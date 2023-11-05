23af049095f099680d4e84a13801fe97a17ec653
asingh1225 <asingh1225@myseneca.ca>
2023-11-05 02:51:39 -0500

    Made The Encrypter And Decryter Working And Now Only Adding Menu option is left..the basic functality is completed

--- C++/Unfinished Project's/Encrypter & Decrypter/main.c
+++ C++/Unfinished Project's/Encrypter & Decrypter/main.c
@@ -1,70 +1,198 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
 #include <string>
-#include <iostream>
 
 using namespace std;
 
-    int main() {
+int main() {
 
-        string alphabet{ "[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" };
-        string key{ " [XZNLWEBGJHQDYVTKFUOMPCIASRxznlwebgjhqdyvtkfuompciasr" };
-
-        string secret_message{};
-
-        cout << "Secret Message: ";
-        getline(cin, secret_message);
-
-        string encrypted_messsage{};
-
-        for (char c : secret_message)
-
-        {
-
-            size_t position = alphabet.find(c);
-
-            if (position != string::npos) {
-
-                char new_char{ key.at(position) };
-                encrypted_messsage += new_char;
-
-            }
-            else
-            {
-                encrypted_messsage += c;
-            }
-
-        }
+	string pre_phrase{};
+	string post_phrase{};
+	string decrypted_phrase{};
 
 
+	cout << "Welcome, To The Loner03 Encrytion System!" << endl;
+	cout << "-----------------------------------------" << endl;
 
-        cout << "\nEncrypted Message: " << encrypted_messsage << endl;
+	char dec{};
+
+	do {
+
+		cout << "Would Your Like To Encrpyt Or Decrypt(E/D) Or Or Press (E) to Exit: ";
+		cin >> dec;
+
+		cout << "\nWould You Like Tou Use Your Own Encrytion Key(Y/N) ";
+		cin >> dec;
+
+		if (dec == 'Y' || dec == 'y')
+		{
+
+			string characters{};
+			string key{};
+
+			cout << "\nKey Construction Rules: " << endl;
+			cout << "-----------------------\n";
+			cout << "1: Make Sure There Are No Repeated Charcters In The Key\n2: Make Sure Both The Key And Solution Has Same Number Of Characters" << endl;
+
+			int number{};
+			char original{};
+			char rep_original{};
+
+			cout << "\nHow Many Characters Are Present In Your Key: ";
+			cin >> number;
+
+			for (size_t i{}; i < number; ++i) {
+
+				cout << "\nEnter Your OriginaL Character: ";
+				cin >> original;
+				cout << "Enter It's Replacement: ";
+				cin >> rep_original;
+
+				characters += original;
+				key += rep_original;
+
+				cout << endl;
+
+				cout << original << " Will Be Replaced By " << rep_original << " After Encrytion." << endl;
+
+				cout << endl;
 
 
-        string decrypted_message{};
-        
-        for (char c : encrypted_messsage) {
+			}
 
-            size_t position = key.find(c);
+			cout << "\nFollowing Encryption Key Has Been Contstructed: ";
+			cout << "\n----------------------------------\n" << endl;
 
-            if (position != string::npos) {
+			for (size_t i{}; i < characters.length(); ++i)
+			{
+				cout << characters.at(i) << " Is Replaced By " << key.at(i) << endl;
+			}
+			cout << "\n----------------------------------" << endl;
 
-                char new_char{ alphabet.at(position) };
-                decrypted_message += new_char;
-            }
-            else
-            {
-                decrypted_message += c;
-            }
-        }
+			cout << "\nPlease Enter The Phrase You Would Like To Encrpt: ";
+			cin.ignore();
+			getline(cin, pre_phrase);
 
-        cout << "\nDecrypted Message: " << decrypted_message;
+			for (char c : pre_phrase)
+
+			{
+				size_t place = characters.find(c);
+
+				if (place != string::npos) {
+
+					char replacement = key.at(place);
+					post_phrase += replacement;
+				}
+
+				else
+				{
+					post_phrase += c;
+				}
+
+			}
+
+			for (char c : post_phrase)
+			{
+				size_t place = key.find(c);
+
+				if (place != string::npos) {
+
+					char replacement = characters.at(place);
+					decrypted_phrase += replacement;
+
+				}
+				else
+				{
+					decrypted_phrase += c;
+				}
+
+			}
 
 
+			cout << "\nOriginal Phrase: " << pre_phrase << endl;
+			cout << "Encrypted Phrase: " << post_phrase << endl;
+			cout << "Decrypted Phrase: " << decrypted_phrase << endl;
 
 
+		}
 
-        cout << endl;
-        return 0;
-    }
+		else if (dec == 'N' or dec == 'n')
+
+
+		{
+
+			string characters{ "1234567890qwertyuioplkjhgfdsazxcvbnm!@#$ %^&*()_+=-~<>?:" };
+			string key{ "mnbvcxz123asdfghjkl098qwepoi56ertyui-=+_{}<>/|][47~$%^&*" };
+
+
+			cout << "\nDefault Encryption Key Used Is The Following: ";
+			cout << "\n----------------------------------\n" << endl;
+
+			for (size_t i{}; i < characters.length(); ++i)
+			{
+				cout << characters.at(i) << " Is Replaced By " << key.at(i) << endl;
+			}
+			cout << "\n----------------------------------" << endl;
+
+			cout << "Please Enter The Phrase You Would Like To Encrpt: ";
+			cin.ignore();
+			getline(cin, pre_phrase);
+
+
+			for (char c : pre_phrase)
+
+			{
+				size_t place = characters.find(c);
+
+				if (place != string::npos) {
+
+					char replacement = key.at(place);
+					post_phrase += replacement;
+				}
+
+				else
+				{
+					post_phrase += c;
+				}
+
+			}
+
+			for (char c : post_phrase)
+			{
+				size_t place = key.find(c);
+
+				if (place != string::npos) {
+
+					char replacement = characters.at(place);
+					decrypted_phrase += replacement;
+
+				}
+				else
+				{
+					decrypted_phrase += c;
+				}
+
+			}
+
+
+			cout << "\nOriginal Phrase: " << pre_phrase << endl;
+			cout << "Encrypted Phrase: " << post_phrase << endl;
+			cout << "Decrypted Phrase: " << decrypted_phrase << endl;
+		}
+
+		else if (dec == 'E' || dec == 'e')
+		{
+
+			cout << "\nThanks For Using Loner03 Encryption System!" << endl;
+			cout << "--------------------------------------------" << endl;
+		}
+
+		else
+		{
+			cout << "\nInvalid Repsonse, Please Try Again!\n";
+		}
+
+	} while (dec != 'E' && dec != 'e');
+
+	cout << endl;
+	return 0;
+}
