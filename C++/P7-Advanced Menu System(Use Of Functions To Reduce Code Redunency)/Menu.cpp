029fc8d0e124b669852e282346cdd79acedaac4f
asingh1225 <asingh1225@myseneca.ca>
2023-11-21 11:34:32 -0500

    Commpleted Advanced Menu System Use Of Functions Learned

--- C++/P7-Advanced Menu System(Use Of Functions To Reduce Code Redunency)/Menu.cpp
+++ C++/P7-Advanced Menu System(Use Of Functions To Reduce Code Redunency)/Menu.cpp
@@ -7,15 +7,19 @@ using namespace std;
 //Function Prototype's Start Here
 
 void main_menu_list();
-void  numberlist_mnakr_intro();
+void numberlist_mnakr_intro();
 void array_size(size_t&);
 void numberarray_maker(vector<int>& v, int n);
 void printVector(const vector<int>& vec);
 void option_choser(char&);
-
+void reinitilize_number_list_intro();
 void print_all_numbers(vector <int>& v);
-void sub_number_menu(char a);
+void sub_number_menu();
 int add_all_numbers(vector <int>& v);
+void ending_sign_line();
+double mean_calculater(vector <int> v);
+int largest_number(vector <int> v);
+int smallest_number(vector <int> v);
 
 //Function Prototype's End's Here
 //Main Function Start's Here
@@ -25,7 +29,7 @@ int main() {
 	size_t count{};
 	double numbers{};
 	char selection{};
-	int sum;
+	int sum{};
 
 	vector <int> integers{};
 
@@ -37,6 +41,9 @@ int main() {
 	{
 		numberlist_mnakr_intro();
 		array_size(count);
+		if (count == 0) {
+			break;
+		}
 		numberarray_maker(integers, count);
 		printVector(integers);
 		option_choser(selection);
@@ -46,53 +53,179 @@ int main() {
 		case 'P':
 		case 'p':
 		{
-			sub_number_menu(selection);
+			sub_number_menu();
 			option_choser(selection);
 
 			if (selection == 'C' || selection == 'c') {
 
+				reinitilize_number_list_intro();
 				numberlist_mnakr_intro();
 				array_size(count);
+				if (count == 0) {
+					break;
+				}
 				numberarray_maker(integers, count);
 				print_all_numbers(integers);
+				ending_sign_line();
 			}
 			else if (selection == 'N' || selection == 'n')
+			{
 				print_all_numbers(integers);
-			else
+				ending_sign_line();
+			}
+			else {
 				cout << "Wrong Input!" << endl;
-			
+				ending_sign_line();
+			}
+
 		}
 		break;
 
 		case 'A':
 		case 'a': {
 
-			sub_number_menu(selection);
+			sub_number_menu();
 			option_choser(selection);
 
 			if (selection == 'C' || selection == 'c') {
 
+				reinitilize_number_list_intro();
 				numberlist_mnakr_intro();
 				array_size(count);
+				if (count == 0) {
+					break;
+				}
 				numberarray_maker(integers, count);
 				print_all_numbers(integers);
 				cout << "Sum: " << add_all_numbers(integers) << endl;
+				ending_sign_line();
 				cout << endl;
 			}
 			else if (selection == 'N' || selection == 'n') {
 				cout << "Sum: " << add_all_numbers(integers) << endl;
+				ending_sign_line();
 				cout << endl;
 			}
-			else
+			else {
 				cout << "Wrong Input!" << endl;
+				ending_sign_line();
+			}
+		}
+				break;
+
+		case 'M' :
+		case 'm' :
+		{
+			sub_number_menu();
+			option_choser(selection);
+
+			if (selection == 'C' || selection == 'c') {
+
+				reinitilize_number_list_intro();
+				numberlist_mnakr_intro();
+				array_size(count);
+				if (count == 0) {
+					break;
+				}
+				numberarray_maker(integers, count);
+				print_all_numbers(integers);
+				cout << "Mean: " << mean_calculater(integers) << endl;
+				ending_sign_line();
+				cout << endl;
+			}
+			else if (selection == 'N' || selection == 'n') {
+				cout << "Mean: " << mean_calculater(integers) << endl;
+				ending_sign_line();
+				cout << endl;
+			}
+			else {
+				cout << "Wrong Input!" << endl;
+				ending_sign_line();
+			}
 		}
 		break;
 
+		case 'S':
+		case 's':
+		{
+			sub_number_menu();
+			option_choser(selection);
+
+			if (selection == 'C' || selection == 'c') {
+
+				reinitilize_number_list_intro();
+				numberlist_mnakr_intro();
+				array_size(count);
+				if (count == 0) {
+					break;
+				}
+				numberarray_maker(integers, count);
+				print_all_numbers(integers);
+				cout << "Smallest Number: " << smallest_number(integers) << endl;
+				ending_sign_line();
+				cout << endl;
+			}
+			else if (selection == 'N' || selection == 'n') {
+				cout << "Smallest Number: " << smallest_number(integers) << endl;
+				ending_sign_line();
+				cout << endl;
+			}
+			else {
+				cout << "Wrong Input!" << endl;
+				ending_sign_line();
+			}
+		}
+		break;
+
+		case 'L':
+		case 'l':
+		{
+			sub_number_menu();
+			option_choser(selection);
+
+			if (selection == 'C' || selection == 'c') {
+
+				reinitilize_number_list_intro();
+				numberlist_mnakr_intro();
+				array_size(count);
+				if (count == 0) {
+					break;
+				}
+				numberarray_maker(integers, count);
+				print_all_numbers(integers);
+				cout << "Largest Number: " << largest_number(integers) << endl;
+				ending_sign_line();
+				cout << endl;
+			}
+			else if (selection == 'N' || selection == 'n') {
+				cout << "Largest Number: " << largest_number(integers) << endl;
+				ending_sign_line();
+				cout << endl;
+			}
+			else {
+				cout << "Wrong Input!" << endl;
+				ending_sign_line();
+			}
+		}
+		break;
+
+		case 'q' :
+		case 'Q': 
+		{//leaving it empty to skip everything and just exit loop
+		}
+
+		default : {
+			cout << "Wrong Input!" << endl;
+			ending_sign_line();
+		}
+				break;
 		}
 
 	} while (selection != 'q' && selection != 'Q');
-	cout << "\nGoodBye!" << endl;
-
+	{
+		cout << "\nGoodBye!" << endl;
+		ending_sign_line();
+	}
 
 	cout << endl;
 	return 0;
@@ -129,7 +262,7 @@ void main_menu_list() {
 
 void numberlist_mnakr_intro() {
 
-	cout << "How Many Number's Would You Like In The List: ";
+	cout << "How Many Number's Would You Like In The List(0 To Quit): ";
 }
 
 void array_size(size_t& n) {
@@ -182,6 +315,11 @@ void option_choser(char& a) {
 
 }
 
+void reinitilize_number_list_intro()
+{
+	cout << "Let's Go Ahead With Making It Again Then, It's Just A Few Simnple Steps Ahead." << endl;
+}
+
 void print_all_numbers(vector <int>& v) {
 
 	cout << "\nHere Is Your Number List: ";
@@ -195,7 +333,7 @@ void print_all_numbers(vector <int>& v) {
 	cout << endl;
 }
 
-void sub_number_menu(char selection) {
+void sub_number_menu() {
 
 	cout << "Sub Menu" << endl;
 	cout << "========" << endl;
@@ -208,7 +346,7 @@ int add_all_numbers(vector <int>& v) {
 
 	int sum{};
 
-	
+
 	for (int c : v) {
 		sum += c;
 
@@ -218,5 +356,61 @@ int add_all_numbers(vector <int>& v) {
 
 }
 
+void ending_sign_line() {
+
+	cout << "\n**************END**************\n" << endl;
+}
+
+double mean_calculater(vector <int> v) {
+
+	int sum{};
+
+	for (auto c : v) {
+
+		sum += c;
+	}
+
+	//using this for mean as it will only chnage in the fuction
+	sum = sum / v.size();
+
+	return sum;
+
+}
+
+int largest_number(vector <int> v) {
+
+	//using sum variable to find largest number as it will only chnage in the function itself
+	int sum{};
+
+	sum = v.at(0);
+	for (auto c : v)
+	{
+		if (sum < c)
+		{
+			sum = c;
+		}
+
+	}
+		
+	return sum;
+}
+
+int smallest_number(vector <int> v) {
+
+	//using sum variable to find smallest number as it will only chnage in the function itself
+	int sum{};
+
+	sum = v.at(0);
+	for (auto c : v)
+	{
+		if (sum > c)
+		{
+			sum = c;
+		}
+
+	}
+
+	return sum;
+}
 
 //Fucntion Defination's Ends Here
